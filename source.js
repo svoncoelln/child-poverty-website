6eedda0bf2907eb26069610729dceca4fcdb8f2b
svoncoelln <svoncoelln@friendsbalt.org>
2022-02-22 10:58:10 -0500

    add to table

--- source.js
+++ source.js
@@ -1,28 +1,7 @@
 function populateTable() {
-var playerList = [
-  {name: "player1", highScore: 1, ability: 8},
-  {name: "player2", highScore: 1, ability: 7},
-  {name: "player3", highScore: 1, ability: 6},
-  {name: "player4", highScore: 1, ability: 5},
-  {name: "player5", highScore: 1, ability: 4},
-  {name: "player6", highScore: 1, ability: 3},
-  {name: "player7", highScore: 1, ability: 2},
-  {name: "player8", highScore: 1, ability: 1}
-];
-
-for (var i = 0; i < playerList.length; i++) {
-  console.log(i);
-  var player = document.getElementById("player" + i + 1);
-  var playerscore = document.getElementById('player' + i + 1 + "score")
-  var progress=Math.random();
-  progress=11*progress;
-  progress=Math.floor(progress);
-  playerList[i].ability=playerList[i].ability+progress;
-  console.log(playerList[i])
-
-  //add players score to the table//
-
-  playerscore.innerText = playerList[i].ability;
-
-}
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(2);
+  for (i = 0; i < 8; i++) {
+    row.insertCell(i);
+  }
 }
