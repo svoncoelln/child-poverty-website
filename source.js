ee371411a6199c8d757d4479d023585d37a50e92
Ren <sren@friendsbalt.org>
2022-02-22 10:29:05 -0500

    change data to the new kind

--- source.js
+++ source.js
@@ -44,3 +44,29 @@ function populateTable() {
   rowNode.appendChild(cellNode);
   table.appendChild(rowNode);
 }
+
+function tableCreate() {
+  const body = document.body,
+        tbl = document.createElement('table');
+  tbl.style.width = '100px';
+  tbl.style.border = '1px solid black';
+
+  for (let i = 0; i < 3; i++) {
+    const tr = tbl.insertRow();
+    for (let j = 0; j < 2; j++) {
+      if (i === 2 && j === 1) {
+        break;
+      } else {
+        const td = tr.insertCell();
+        td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
+        td.style.border = '1px solid black';
+        if (i === 1 && j === 1) {
+          td.setAttribute('rowSpan', '2');
+        }
+      }
+    }
+  }
+  body.appendChild(tbl);
+}
+
+tableCreate();
