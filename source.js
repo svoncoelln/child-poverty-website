0c20f203a03c05cce968513850b2d989bb63edde
svoncoelln <svoncoelln@friendsbalt.org>
2022-02-25 09:58:10 -0500

    add ajax

--- source.js
+++ source.js
@@ -1,3 +1,30 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("test").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "data.txt", true);
+  xhttp.send();
+}
+
+const linesArray = loadDoc().split("\n");
+const objArray;
+for (let i = 0; i < linesArray.length; i++) {
+  const current = linesArray[i].split(",");
+  const myObj = {
+    state: current[0],
+    FIPS: current[1],
+    id: current[2],
+    name: current[3],
+    population: current[4],
+    children: current[5],
+    impoverishedChildren: current[6],
+    percentage: (current[6]/current[5])*100+"%"
+  }
+  objArray.push(myObj);
+}
 
 function loadTableData(list) {
 	let table = document.getElementById("table-body");
@@ -8,32 +35,35 @@ function loadTableData(list) {
 		x.innerHTML = list[i];
 	}
 }
-let l = ["AL", "01", "00190", "Alabaster City School District", "35,268", "6,797", "669", "9.84%"];
-let l2 = ["AL","1","5","Albertville City School District","22120","4163","918"];
-let l3 = ["AL","1","30","Alexander City City School District","16819","2579","700"];
-let l4 = ["AL","1","60","Andalusia City School District","8818","1471","346"];
-let l5 = ["AL","1","90","Anniston City School District","22017","3053","735"];
-let l6 = ["AL","1","100","Arab City School District","8442","1537","218"];
-let l7 = ["AL","1","120","Athens City School District","27028","4124","585"];
-let l8 = ["AL","1","180","Attalla City School District","6006","942","226"];
-let l9 = ["AL","1","210","Auburn City School District","63511","7514","655"];
-let l10 = ["AL","1","240","Autauga County School District","56145","9647","1378"];
-loadTableData(l);
-loadTableData(l2);
-loadTableData(l3);
-loadTableData(l4);
-loadTableData(l5);
-loadTableData(l6);
-loadTableData(l7);
-loadTableData(l8);
-loadTableData(l9);
-loadTableData(l10);
 
-let myArray = [l, l2, l3, l4, l5, l6, l7, l8, l9, l10];
+loadTableData(objArray);
 
-function sortData(arr) {
-  arr.sort();
-}
+// let l = ["AL", "01", "00190", "Alabaster City School District", "35,268", "6,797", "669", "9.84%"];
+// let l2 = ["AL","1","5","Albertville City School District","22120","4163","918"];
+// let l3 = ["AL","1","30","Alexander City City School District","16819","2579","700"];
+// let l4 = ["AL","1","60","Andalusia City School District","8818","1471","346"];
+// let l5 = ["AL","1","90","Anniston City School District","22017","3053","735"];
+// let l6 = ["AL","1","100","Arab City School District","8442","1537","218"];
+// let l7 = ["AL","1","120","Athens City School District","27028","4124","585"];
+// let l8 = ["AL","1","180","Attalla City School District","6006","942","226"];
+// let l9 = ["AL","1","210","Auburn City School District","63511","7514","655"];
+// let l10 = ["AL","1","240","Autauga County School District","56145","9647","1378"];
+// loadTableData(l);
+// loadTableData(l2);
+// loadTableData(l3);
+// loadTableData(l4);
+// loadTableData(l5);
+// loadTableData(l6);
+// loadTableData(l7);
+// loadTableData(l8);
+// loadTableData(l9);
+// loadTableData(l10);
+
+// let myArray = [l, l2, l3, l4, l5, l6, l7, l8, l9, l10];
+
+// function sortData(arr) {
+//   arr.sort();
+// } 
 
 /*
 function populateTable() {
