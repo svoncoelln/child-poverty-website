204882c4b00f9d65fdb6863dd154d7bed5190d6a
Banerjee <kbanerjee@friendsbalt.org>
2022-03-25 09:23:15 -0400

    together

--- source.js
+++ source.js
@@ -24,9 +24,11 @@ function loadTablelist(list) {
   document.getElementById("tableBody").innerHTML = tableString;
 }
 
+const objArray;
+
 function read(text) {
   let linesArray = text.split("\n");
-  const objArray = [];
+  objArray = [];
   for (let i = 0; i < linesArray.length; i++) {
     const current = linesArray[i].split(",");
     const myObj = {
@@ -41,7 +43,7 @@ function read(text) {
     }
     objArray.push(myObj);
   }
-  loadTablelist(objArray);
+  loadNext();
 }
 
 function changenn(){
@@ -99,55 +101,49 @@ function search(){
 	
 	let target = (document.getElementById('searchfor').value).toString();
 	
-	for(let i = 0; i < list.length; i++) {
+	for(let i = 0; i < objArray.length; i++) {
 		let region = "";
 		if(column == 'state'){
-			region = list[i].state;
+			region = objArray[i].state;
 		}
 		else if(column == 'FIPS'){
-			region = list[i].FIPS;
+			region = objArray[i].FIPS;
 		}
 		else if(column == 'dist-id'){
-			region = list[i].id;
+			region = objArray[i].id;
 		}
 		else if(column == 'name'){
-			region = list[i].name;
-			console.log(region);
+			region = objArray[i].name;
 		}
 		else if(column == 'population'){
-			region = list[i].population;
+			region = objArray[i].population;
 		}
 		else if(column == 'children'){
-			region = list[i].children;
+			region = objArray[i].children;
 		}
 		else if(column == 'impoverishedchildren'){
-			region = list[i].impoverishedchildren;
+			region = objArray[i].impoverishedchildren;
 		}
 		else if(column == 'percentage'){
-			region = list[i].percentage;
+			region = objArray[i].percentage;
 		}
 
 		if(scope == 'exact'){
 			if(region == target){
-				loadTablelist(list[i]);
+				loadTablelist(objArray[i]);
 			}
 		}
 		else if(scope == 'contains'){
 			if(region.includes(target)){
-				loadTablelist(list[i]);
+				loadTablelist(objArray[i]);
 			}
 		}
 		else if(scope == 'not-contains'){
 			if(!region.includes(target)){
-				loadTablelist(list[i]);
+				loadTablelist(objArray[i]);
 			}
 		}
 	}
-}
-//to sort take first part of search as findcolumn then convert the column to an array?
-
-for(let i = 0; i < list.length; i++){
-	list[i] = arrToregion(list[i]);
 }
 
 loadNext();
@@ -156,32 +152,80 @@ function loadNext() {
 	del();
 	changenn();
 	for(let i = counter; i < counter+nn; i++) {
-		if(i < 0 || i > list.length) {
+		if(i < 0 || i > objArray.length) {
 			i = 0;
 			counter = 0;
 		}
-		loadTablelist(list[i]);
+		loadTablelist(objArray[i]);
 	}
 	counter+=nn;
-	if(counter < 0 || counter > list.length)
+	if(counter < 0 || counter > objArray.length)
 		counter = 0;
 }
 
 function loadPrevious() {
 	del();
 	changenn();
-	if(counter-nn < 0 || counter-nn > list.length) {
+	if(counter-nn < 0 || counter-nn > objArray.length) {
 		i = 0;
 		counter = nn;
 	}
 	for(let i = counter-nn; i < counter; i++) {
-		if(i < 0 || i > list.length) {
+		if(i < 0 || i > objArray.length) {
 			i = 0;
 			counter = nn;
 		}
-		loadTablelist(list[i]);
+		loadTablelist(objArray[i]);
 	}
 	counter-=nn;
-	if(counter < 0 || counter > list.length)
+	if(counter < 0 || counter > objArray.length)
 		counter = 0;
+}
+
+function compareState(a, b){
+	return(a.state.localeCompare(b.state));
+}
+
+function compareName(a, b){
+	return(a.name.localeCompare(b.name));
+}
+
+function comparePopulation(a, b){
+	if(a.population > b.population){
+		return 1;
+	}
+	else if(a.population < b.population){
+		return -1;
+	}
+	return 0;
+}
+
+function compareChildren(a, b){
+	if(a.children > b.children){
+		return 1;
+	}
+	else if(a.children < b.children){
+		return -1;
+	}
+	return 0;
+}
+
+function compareImpoverishedChildren(a, b){
+	if(a.impoverishedchildren > b.impoverishedchildren){
+		return 1;
+	}
+	else if(a.impoverishedchildren < b.impoverishedchildren){
+		return -1;
+	}
+	return 0;
+}
+
+function comparePercentage(a, b){
+	if(a.percentage > b.percentage){
+		return 1;
+	}
+	else if(a.percentage < b.percentage){
+		return -1;
+	}
+	return 0;
 }
